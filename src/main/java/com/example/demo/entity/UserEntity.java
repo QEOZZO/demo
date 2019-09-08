34a00882be8746835d9e40aaa94a10d4061d150b
QEOZZO <chaiqianlong@126.com>
2019-09-08 17:52:05 +0800

    用户信息实体类

---
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,45 @@
+package com.example.demo.entity;
+
+/**
+ * 用户信息实体类
+ * @Author: chaiqianlong
+ * @Date: 2019-09-08 17:50
+ */
+public class UserEntity {
+    private Integer id;
+    private String userName;
+    private String passWord;
+    private String realName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+}
