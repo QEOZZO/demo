46d3a7f93ecebe92b7edccfe60d0e32c5ed32a66
QEOZZO <chaiqianlong@126.com>
2019-10-30 16:36:46 +0800

    xml解析为java bean

--- src/main/java/com/example/demo/xml/service/impl/DemoServiceImpl.java
+++ src/main/java/com/example/demo/xml/service/impl/DemoServiceImpl.java
@@ -12,21 +12,19 @@ import com.example.demo.xml.service.DemoService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service(value = "demoService")
 public class DemoServiceImpl extends BaseServiceImpl implements DemoService {
     @Override
     public MsgModel testSave(MainDataBean mainDataBean) {
         MsgModel msgModel = new MsgModel();
         UserDataBean userDataBean = mainDataBean.getUserDataBean();
-        Set<UserBean> userBeans = userDataBean.getUserBeans();
         boolean isError = false;
         String errorMsg = "";
-        for (UserBean userBean : userBeans) {
+        for (UserBean userBean : userDataBean.getUserBeans()) {
             try {
                 UserEntity userEntity = new UserEntity();
                 BeanUtils.copyProperties(userBean, userEntity, getIgnoreProperties(userBean, TypeBean.FieldType.USER));
+                System.out.println("111111");
             } catch (Exception e) {
                 isError = true;
                 errorMsg += e.getMessage() + "<br>";
